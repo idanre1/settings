a47dd6bd74505e06ca2b01ddb0b8dd5cbf457417
Idan Regev <idanre1@gmail.com>
2021-11-11 22:13:03 +0200

    rapids env alias

--- python_init.sh
+++ python_init.sh
@@ -5,6 +5,9 @@ alias nb='jupyter lab &'
 py3bin=/home/$USER/py3env/bin
 alias py3env='cd $py3bin; source activate; cd -'
 
-# mlfinlab python virtualenc
+# mlfinlab python virtualenv
 mlfinlabbin=/home/$USER/py3mlfinlab/bin
 alias py3mlfinlab='cd $mlfinlabbin; source activate; cd -'
+
+# rapids python virtualenv
+alias py3rapids='source ~/miniconda3/etc/profile.d/conda.sh; conda activate rapids-21.10'
