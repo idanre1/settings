2947cbc4ed4e63f63a9ab0b979535839abada8df
Idan Regev <idanre1@gmail.com>
2022-03-13 10:28:01 +0200

    cuda venv

--- python_init.sh
+++ python_init.sh
@@ -8,7 +8,7 @@ py3torchbin=/home/$USER/Envs/py3torch/bin
 alias py3torch='cd $py3torchbin; source activate; cd -'
 
 # rapids python virtualenv
-alias py3gpu='source ~/miniconda3/etc/profile.d/conda.sh; conda activate rapids-21.10'
+alias py3gpu='source ~/miniconda3/etc/profile.d/conda.sh; conda activate cuda_11.4'
 
 # poetry
 alias poetry=/home/$USER/.local/bin/poetry
